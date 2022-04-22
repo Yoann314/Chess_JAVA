a3232ed60ab7348ab904fe44e4a7216989e4bf8c
Yoann Fraysse <yofraysse@gmail.com>
2022-04-22 11:58:24 +0200

    ajout des pieces

--- src/EchecFrPl/Plateau.java
+++ src/EchecFrPl/Plateau.java
@@ -17,9 +17,26 @@ public class Plateau {
 		pieces[0][2] = new Fou(0,2);
 		pieces[0][3] = new Reine(0,3);
 		pieces[0][4] = new Roi(0,4);
+		pieces[0][5] = new Fou(0,5);
+		pieces[0][6] = new Cavalier(0,6);
+		pieces[0][7] = new Tour(0,7);
 
+		for(int i = 8; i < 16; i++){
+			pieces[0][i] = new Pion(1,i);
+		}
 
-		pieces[1][0] = new Tour(7,7);
+		for(int i = 8; i < 16; i++){
+			pieces[1][i] = new Pion(6,i);
+		}
+
+		pieces[1][0] = new Tour(7,0);
+		pieces[1][1] = new Cavalier(7,1);
+		pieces[1][2] = new Fou(7,2);
+		pieces[1][3] = new Reine(7,3);
+		pieces[1][4] = new Roi(7,4);
+		pieces[1][5] = new Fou(7,5);
+		pieces[1][6] = new Cavalier(7,6);
+		pieces[1][7] = new Tour(7,7);
 
 		for(int indiceLigne = 3; indiceLigne = 6; indiceLigne++) {
 			for(int indiceColonne = 0; indiceColonne < grille[indiceLigne].length; indiceColonne++) {
