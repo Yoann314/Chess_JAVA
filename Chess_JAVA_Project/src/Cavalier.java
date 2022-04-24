bafe8ee2d3e3b7ff6badea32cc782d77d39ce0a5
Yoann Fraysse <yofraysse@gmail.com>
2022-04-24 18:03:38 +0200

    initial commit

---
+++ Chess_JAVA_Project/src/Cavalier.java
@@ -0,0 +1,10 @@
+package EchecFrPl;
+
+public class Cavalier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chess_JAVA_Project/src/Pion.java
@@ -0,0 +1,10 @@
+package EchecFrPl;
+
+public class Pion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
