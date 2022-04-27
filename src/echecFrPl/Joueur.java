e913f17d9b8ed7dab468cff5a9d5049f587292f2
Yoann Fraysse <yofraysse@gmail.com>
2022-04-27 10:24:38 +0200

    yo

---
+++ src/echecFrPl/Joueur.java
@@ -0,0 +1,20 @@
+package EchecFrPl;
+
+import javax.swing.JOptionPane;
+
+public class Joueur {
+	String pseudo;
+	
+	public Joueur() {		
+		//.this, "Quel est le nom du Joueur ?", 
+		//JOptionPane.OK_OPTION, JOptionPane.INPUT_VALUE_PROPERTY);
+	}
+
+	public Joueur(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/echecFrPl/MonBouton.java
@@ -0,0 +1,31 @@
+package echecFrPl;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MonBouton extends JButton {
+    private Color bg;
+    Image img;
+
+    public MonBouton() {
+        super();
+        img = null;
+    }
+    
+    public MonBouton(Image i) {
+        super();
+        img = i;
+    }
+
+    public void setBackground(Color background){
+        bg = background;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(bg);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        if(img!=null)
+            g.drawImage(img, 0, 0, null);
+    }
+}
