9affa3837b3f68ab07abdc65f03593fd8ea012ce
magdalenq <78900119+magdalenq@users.noreply.github.com>
2022-04-29 11:40:08 +0200

    Update Fou.java

--- src/echecFrPl/Fou.java
+++ src/echecFrPl/Fou.java
@@ -6,10 +6,11 @@ public class Fou extends Piece{
 	public Fou(String c, int img) {
 		super(img);
 		this.setCouleur(c);
+		this.setForme("fou");
 		this.estActif=true;
 		this.value = 3;
 	}
-		public boolean deplacementValide(int indLigneDepart, int indColDepart, int indLigneArrive, int indColArrive) // nouvelles coordonnees 
+		public boolean deplacementValide(int indLigneDepart, int indColDepart, int indLigneArrive, int indColArrive, String c) // nouvelles coordonnees 
 		{
 			if (indLigneArrive < 8 && indColArrive < 8) {		
 				if (indLigneArrive == indLigneDepart && indColArrive == indColDepart) return false; //le piece n'a pas bouge
